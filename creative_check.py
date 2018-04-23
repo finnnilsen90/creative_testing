f9001e807b8c9f041a6c41e5911332fa93de3a66
finnnilsen90 <fnilsen1051@gmail.com>
2018-04-23 10:01:00 -0700

    updated messaging

--- creative_check.py
+++ creative_check.py
@@ -7,6 +7,7 @@ for filename in glob.glob('*.jpg'):
     image=Image.open(filename)
     path = os.path.abspath(filename)
 
+    print(str(image.filename) + ' CURRENT format is => ' + str(image.mode))
     if image.mode == 'CMYK': 
         image.convert('RGB').save(path)
         print(str(image.filename) + ' format is now => ' + str(image.mode))
